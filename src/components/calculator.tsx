9e1e7d6124686a4594a5620770ba2a256c92efd2
Aberkane Younes <y.aberkane@afelio.be>
2023-07-24 17:34:32 +0200

    useref

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Calculator = () => {
 
+	let total = useRef(0);
+
 	const initialValues = {
-		initialPrice: 0,
-		fee1: 0,
-		fee2: 0,
-		total: 0
+		initialCost: 0,
+		shippingCost: 0,
+		listingCost: 0.2,
+		relestingAfterSaleCost: 0.2,
+		processingCost: 0.3
 	};
 
 	const [state, setState] = useState(initialValues);
@@ -18,19 +21,85 @@ const Calculator = () => {
 			...state,
 			[name]: floatValue,
 		};
+		newState.processingCost = (0.04 * state.initialCost) + 0.3;
 		setState(newState);
 	};
+
+	useEffect(() => {
+		total.current = +(state.initialCost + state.shippingCost + state.listingCost + state.relestingAfterSaleCost + state.processingCost).toPrecision(4);
+	}, [state]);
 	
 	return (
 		<form>
-			<p>prix de fabrication</p>
-			<input name='initialPrice' onChange={handleInputChange} type="number" min={0} step={0.01} defaultValue={state.initialPrice}></input>
-			<p>frais 1</p>
-			<input name='fee1' onChange={handleInputChange} type="number" min={0} step={0.01} defaultValue={state.fee1}></input>
-			<p>frais 2</p>
-			<input name='fee2' onChange={handleInputChange} type="number" min={0} step={0.01} defaultValue={state.fee2}></input>
-			<p>total</p>
-			<input name='total' type="number" min={0} value={(state.initialPrice + state.fee1 + state.fee2).toPrecision(2)} disabled></input>
+			<div>
+				<span className="mr-2">Fabrication</span>
+				<input
+					name='initialCost'
+					type='number'
+					min={0}
+					step={0.01}
+					required
+					className='invalid:border border-red-500'
+					defaultValue={state.initialCost}
+					onChange={handleInputChange}
+				></input>
+			</div>
+			<div>
+				<span className="mr-2">Livraison</span>
+				<input
+					name='shippingCost'
+					type='number'
+					min={0}
+					step={0.01}
+					required
+					className='invalid:border border-red-500'
+					defaultValue={state.shippingCost}
+					onChange={handleInputChange}
+				></input>
+			</div>
+			<div>
+				<span className="mr-2">Mise en vente</span>
+				<input
+					name='listingCost'
+					type='number'
+					min={0}
+					step={0.01}
+					disabled
+					defaultValue={state.listingCost}
+				></input>
+			</div>
+			<div>
+				<span className='mr-2'>Remise en vente après vente</span>
+				<input
+					name='relestingAfterSaleCost'
+					type='number'
+					min={0}
+					step={0.01}
+					disabled
+					defaultValue={state.relestingAfterSaleCost}
+				></input>
+			</div>
+			<div>
+				<span className='mr-2'>Frais de traitement (4% + total de la commande + 30c)</span>
+				<input
+					name='processingCost'
+					type='number'
+					min={0}
+					step={0.01}
+					disabled
+					value={state.processingCost}
+				></input>
+			</div>
+			<div>
+				<span className='mr-2'>Total</span>
+				<input
+					name='total'
+					type='number'
+					min={0}
+					disabled
+					value={total.current}
+				></input>
+			</div>
 		</form>
 	);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
 
       <main className="min-h-screen flex items-center justify-center bg-slate-50">
         <div>
-          <h1 className="text-3xl font-semibold mb-6">Etsy Cost Calculator</h1>
+          {/* <h1 className="text-3xl font-semibold mb-6">Etsy Cost Calculator</h1> */}
           <Calculator />
         </div>
       </main>
